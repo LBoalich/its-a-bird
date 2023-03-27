4b192a59fe5843bb198ea7dee025422903c3775a
LBoalich <lkboalich@gmail.com>
2023-03-27 15:10:18 -0400

    Add questions and answers to quiz slice state

--- src/features/quiz/quizSlice.js
+++ src/features/quiz/quizSlice.js
@@ -8,48 +8,248 @@ const initialState = {
       choose: [
         {
           "img": "./quizPics/citron_crested.jpg",
-          "alt": "citron crested",
-          "answer": "citron crested",
+          "alt": "Red",
+          "answer": ["Palm", "Gang Gang", "Red-tailed Black"],
         },
         {
           "img": "./quizPics/major_mitchell.jpg",
-          "alt": "major mitchell",
-          "answer": "major mitchell",
+          "alt": "Yellow",
+          "answer": ["Sulphur Crested", "Cockatiel", "Yellow-tailed Black", "Umbrella"],
         },
         {
           "img": "./quizPics/galah2.jpg",
-          "alt": "galah",
-          "answer": "galah",
+          "alt": "Blue",
+          "answer": ["Blue Eyed"],
         },
         {
           "img": "./quizPics/red_tailed_black.jpg",
-          "alt": "red-tailed black",
-          "answer": "red-tailed black",
+          "alt": "Pink",
+          "answer": ["Major Mitchell", "Galah"],
         },
       ],
     },
     2: {
-      question: "question2",
+      question: "Would You Rather Be A",
       choose: [
         {
           "img": "./quizPics/red_tailed_black.jpg",
-          "alt": "red-tailed black",
-          "answer": "red-tailed black",
+          "alt": "Jockey",
+          "answer": ["Cockatiel"],
         },
         {
           "img": "./quizPics/citron_crested.jpg",
-          "alt": "citron crested",
-          "answer": "citron crested",
+          "alt": "Sumo Wrestler",
+          "answer": ["Palm"],
         },
         {
           "img": "./quizPics/galah2.jpg",
-          "alt": "galah",
-          "answer": "galah",
+          "alt": "Gymnast",
+          "answer": ["Umbrella"],
         },
         {
           "img": "./quizPics/major_mitchell.jpg",
-          "alt": "major mitchell",
-          "answer": "major mitchell",
+          "alt": "Comedian",
+          "answer": ["Galah", "Gang Gang"],
+        },
+      ],
+    },
+    3: {
+      question: "What Is Your Personality Most Like?",
+      choose: [
+        {
+          "img": "./quizPics/red_tailed_black.jpg",
+          "alt": "Shy",
+          "answer": ["Red-tailed Black"],
+        },
+        {
+          "img": "./quizPics/citron_crested.jpg",
+          "alt": "Loud",
+          "answer": ["Yellow-tailed Black", "Sulphur Crested"],
+        },
+        {
+          "img": "./quizPics/galah2.jpg",
+          "alt": "Quiet",
+          "answer": ["Gang Gang", "Galah"],
+        },
+        {
+          "img": "./quizPics/major_mitchell.jpg",
+          "alt": "Friendly",
+          "answer": ["Blue Eyed"],
+        },
+      ],
+    },
+    4: {
+      question: "Which Gemstone Do You Like Best?",
+      choose: [
+        {
+          "img": "./quizPics/red_tailed_black.jpg",
+          "alt": "Black Opal",
+          "answer": ["Palm", "Galah", "Blue Eyed", "Major Mitchell"],
+        },
+        {
+          "img": "./quizPics/citron_crested.jpg",
+          "alt": "Quartz",
+          "answer": ["Umbrella", "Sulphur Crested"],
+        },
+        {
+          "img": "./quizPics/galah2.jpg",
+          "alt": "Blue Diamond",
+          "answer": ["Red-tailed Black", "Yellow-tailed Black"],
+        },
+        {
+          "img": "./quizPics/major_mitchell.jpg",
+          "alt": "Amethyst",
+          "answer": ["Cockatiel", "Gang Gang"],
+        },
+      ],
+    },
+    5: {
+      question: "Would You Rather",
+      choose: [
+        {
+          "img": "./quizPics/red_tailed_black.jpg",
+          "alt": "Sing",
+          "answer": ["Cockatiel"],
+        },
+        {
+          "img": "./quizPics/citron_crested.jpg",
+          "alt": "Dance",
+          "answer": ["Sulphur Crested"],
+        },
+        {
+          "img": "./quizPics/galah2.jpg",
+          "alt": "Drum",
+          "answer": ["Palm"],
+        },
+        {
+          "img": "./quizPics/major_mitchell.jpg",
+          "alt": "Scream",
+          "answer": ["Major Mitchell"],
+        },
+      ],
+    },
+    6: {
+      question: "What Is Your Style",
+      choose: [
+        {
+          "img": "./quizPics/red_tailed_black.jpg",
+          "alt": "Goth",
+          "answer": ["Red-tailed Black", "Yellow-tailed Black"],
+        },
+        {
+          "img": "./quizPics/citron_crested.jpg",
+          "alt": "Tuxedo Suit",
+          "answer": ["Palm"],
+        },
+        {
+          "img": "./quizPics/galah2.jpg",
+          "alt": "Classic",
+          "answer": ["Umbrella", "Sulphur Crested", "Blue Eyed"],
+        },
+        {
+          "img": "./quizPics/major_mitchell.jpg",
+          "alt": "Colorful",
+          "answer": ["Major Mitchell", "Cockatiel", "Galah", "Gang Gang"],
+        },
+      ],
+    },
+    7: {
+      question: "You Are Lost.  Which Direction Do You Take?",
+      choose: [
+        {
+          "img": "./quizPics/red_tailed_black.jpg",
+          "alt": "North",
+          "answer": ["Red-tailed Black", "Palm", "Blue Eyed", "Umbrella"],
+        },
+        {
+          "img": "./quizPics/citron_crested.jpg",
+          "alt": "East",
+          "answer": ["Yellow-tailed Black", "Sulphur Crested"],
+        },
+        {
+          "img": "./quizPics/galah2.jpg",
+          "alt": "South",
+          "answer": ["Gang Gang"],
+        },
+        {
+          "img": "./quizPics/major_mitchell.jpg",
+          "alt": "West",
+          "answer": ["Major Mitchell", "Cockatiel", "Galah"],
+        },
+      ],
+    },
+    8: {
+      question: "Which Food Or Drink Do You Want?",
+      choose: [
+        {
+          "img": "./quizPics/red_tailed_black.jpg",
+          "alt": "Wine",
+          "answer": ["Gang Gang"],
+        },
+        {
+          "img": "./quizPics/citron_crested.jpg",
+          "alt": "Cheese",
+          "answer": ["Cockatiel"],
+        },
+        {
+          "img": "./quizPics/galah2.jpg",
+          "alt": "Steak",
+          "answer": ["Umbrella"],
+        },
+        {
+          "img": "./quizPics/major_mitchell.jpg",
+          "alt": "Peanuts",
+          "answer": ["Red-tailed Black"],
+        },
+      ],
+    },
+    9: {
+      question: "Pick A Number",
+      choose: [
+        {
+          "img": "./quizPics/red_tailed_black.jpg",
+          "alt": "2",
+          "answer": ["Gang Gang", "Cockatiel"],
+        },
+        {
+          "img": "./quizPics/citron_crested.jpg",
+          "alt": "4",
+          "answer": ["Palm", "Red-tailed Black", "Yellow-tailed Black"],
+        },
+        {
+          "img": "./quizPics/galah2.jpg",
+          "alt": "8",
+          "answer": ["Umbrella", "Blue Eyed", "Sulphur Crested"],
+        },
+        {
+          "img": "./quizPics/major_mitchell.jpg",
+          "alt": "5",
+          "answer": ["Major Mitchell", "Galah"],
+        },
+      ],
+    },
+    10: {
+      question: "What Is The Pretiest Landscape",
+      choose: [
+        {
+          "img": "./quizPics/red_tailed_black.jpg",
+          "alt": "Forest",
+          "answer": ["Gang Gang", "Sulphur Crested", "Blue Eyed", "Yellow-tailed Black", "Major Mitchell"],
+        },
+        {
+          "img": "./quizPics/citron_crested.jpg",
+          "alt": "Field",
+          "answer": ["Galah", "Cockatiel"],
+        },
+        {
+          "img": "./quizPics/galah2.jpg",
+          "alt": "Grove",
+          "answer": ["Umbrella"],
+        },
+        {
+          "img": "./quizPics/major_mitchell.jpg",
+          "alt": "Rainforest",
+          "answer": ["Palm", "Red-tailed Black"],
         },
       ],
     },
