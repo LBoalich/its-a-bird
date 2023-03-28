292667c0e61a95c414dea5890b2619e202cb536b
LBoalich <lkboalich@gmail.com>
2023-03-28 18:33:54 -0400

    Set up Results feature

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import quizReducer from '../features/quiz/quizSlice';
+import resultsReducer from "../features/results/resultsSlice";
 
 export const store = configureStore({
   reducer: {
     quiz: quizReducer,
+    results: resultsReducer,
   },
 });
---
+++ src/features/results/resultsSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const resultsSlice = createSlice({
+    name: 'results',
+    initialState,
+    reducers: {},
+});
+
+export default resultsSlice.reducer;
