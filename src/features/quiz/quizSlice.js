16dc8f0d07ec58406c3250601355f3807a02a79b
LBoalich <lkboalich@gmail.com>
2023-03-28 14:21:33 -0400

    Add pictures to quiz questions

--- src/features/quiz/quizSlice.js
+++ src/features/quiz/quizSlice.js
@@ -7,22 +7,22 @@ const initialState = {
       question: "Pick A Color",
       choose: [
         {
-          "img": "./quizPics/citron_crested.jpg",
-          "alt": "Red",
-          "answer": ["Palm", "Gang Gang", "Red-tailed Black"],
-        },
-        {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/1/yellow.jpg",
           "alt": "Yellow",
           "answer": ["Sulphur Crested", "Cockatiel", "Yellow-tailed Black", "Umbrella"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/1/blue.jpg",
           "alt": "Blue",
           "answer": ["Blue Eyed"],
         },
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/1/red.jpg",
+          "alt": "Red",
+          "answer": ["Palm", "Gang Gang", "Red-tailed Black"],
+        },
+        {
+          "img": "./quizPics/1/pink.jpg",
           "alt": "Pink",
           "answer": ["Major Mitchell", "Galah"],
         },
@@ -32,22 +32,22 @@ const initialState = {
       question: "Would You Rather Be A",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/2/jockey.png",
           "alt": "Jockey",
           "answer": ["Cockatiel"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
+          "img": "./quizPics/2/sumo.png",
           "alt": "Sumo Wrestler",
           "answer": ["Palm"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/2/gymnast.png",
           "alt": "Gymnast",
           "answer": ["Umbrella"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/2/comedian.jpg",
           "alt": "Comedian",
           "answer": ["Galah", "Gang Gang"],
         },
@@ -57,22 +57,22 @@ const initialState = {
       question: "What Is Your Personality Most Like?",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/3/shy.jpg",
           "alt": "Shy",
           "answer": ["Red-tailed Black"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
+          "img": "./quizPics/3/loud.png",
           "alt": "Loud",
           "answer": ["Yellow-tailed Black", "Sulphur Crested"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/3/quiet.png",
           "alt": "Quiet",
           "answer": ["Gang Gang", "Galah"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/3/friendly.png",
           "alt": "Friendly",
           "answer": ["Blue Eyed"],
         },
@@ -82,22 +82,22 @@ const initialState = {
       question: "Which Gemstone Do You Like Best?",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/4/blackOpal.png",
           "alt": "Black Opal",
           "answer": ["Palm", "Galah", "Blue Eyed", "Major Mitchell"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
+          "img": "./quizPics/4/quartz.png",
           "alt": "Quartz",
           "answer": ["Umbrella", "Sulphur Crested"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/4/blueDiamond.png",
           "alt": "Blue Diamond",
           "answer": ["Red-tailed Black", "Yellow-tailed Black"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/4/amethyst.png",
           "alt": "Amethyst",
           "answer": ["Cockatiel", "Gang Gang"],
         },
@@ -107,22 +107,22 @@ const initialState = {
       question: "Would You Rather",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/5/sing.jpg",
           "alt": "Sing",
           "answer": ["Cockatiel"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
+          "img":  "./quizPics/5/dance.jpg",
           "alt": "Dance",
           "answer": ["Sulphur Crested"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img":  "./quizPics/5/drum.jpg",
           "alt": "Drum",
           "answer": ["Palm"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/5/scream.png",
           "alt": "Scream",
           "answer": ["Major Mitchell"],
         },
@@ -132,22 +132,22 @@ const initialState = {
       question: "What Is Your Style",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/6/goth.jpg",
           "alt": "Goth",
           "answer": ["Red-tailed Black", "Yellow-tailed Black"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
-          "alt": "Tuxedo Suit",
-          "answer": ["Palm"],
-        },
-        {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/6/classic.jpg",
           "alt": "Classic",
           "answer": ["Umbrella", "Sulphur Crested", "Blue Eyed"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/6/tuxedoSuit.jpg",
+          "alt": "Tuxedo Suit",
+          "answer": ["Palm"],
+        },
+        {
+          "img": "./quizPics/6/colorful.jpg",
           "alt": "Colorful",
           "answer": ["Major Mitchell", "Cockatiel", "Galah", "Gang Gang"],
         },
@@ -157,22 +157,22 @@ const initialState = {
       question: "You Are Lost.  Which Direction Do You Take?",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/7/north.png",
           "alt": "North",
           "answer": ["Red-tailed Black", "Palm", "Blue Eyed", "Umbrella"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
+          "img": "./quizPics/7/east.png",
           "alt": "East",
           "answer": ["Yellow-tailed Black", "Sulphur Crested"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/7/south.png",
           "alt": "South",
           "answer": ["Gang Gang"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/7/west.png",
           "alt": "West",
           "answer": ["Major Mitchell", "Cockatiel", "Galah"],
         },
@@ -182,22 +182,22 @@ const initialState = {
       question: "Which Food Or Drink Do You Want?",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/8/wine.png",
           "alt": "Wine",
           "answer": ["Gang Gang"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
+          "img": "./quizPics/8/cheese.png",
           "alt": "Cheese",
           "answer": ["Cockatiel"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/8/steak.png",
           "alt": "Steak",
           "answer": ["Umbrella"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/8/peanuts.png",
           "alt": "Peanuts",
           "answer": ["Red-tailed Black"],
         },
@@ -207,22 +207,22 @@ const initialState = {
       question: "Pick A Number",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/9/2.png",
           "alt": "2",
           "answer": ["Gang Gang", "Cockatiel"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
+          "img": "./quizPics/9/4.png",
           "alt": "4",
           "answer": ["Palm", "Red-tailed Black", "Yellow-tailed Black"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/9/8.png",
           "alt": "8",
           "answer": ["Umbrella", "Blue Eyed", "Sulphur Crested"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/9/5.png",
           "alt": "5",
           "answer": ["Major Mitchell", "Galah"],
         },
@@ -232,22 +232,22 @@ const initialState = {
       question: "What Is The Pretiest Landscape",
       choose: [
         {
-          "img": "./quizPics/red_tailed_black.jpg",
+          "img": "./quizPics/10/forest.jpg",
           "alt": "Forest",
           "answer": ["Gang Gang", "Sulphur Crested", "Blue Eyed", "Yellow-tailed Black", "Major Mitchell"],
         },
         {
-          "img": "./quizPics/citron_crested.jpg",
+          "img": "./quizPics/10/field.jpg",
           "alt": "Field",
           "answer": ["Galah", "Cockatiel"],
         },
         {
-          "img": "./quizPics/galah2.jpg",
+          "img": "./quizPics/10/mangrove.jpg",
           "alt": "Grove",
           "answer": ["Umbrella"],
         },
         {
-          "img": "./quizPics/major_mitchell.jpg",
+          "img": "./quizPics/10/rainforest.jpg",
           "alt": "Rainforest",
           "answer": ["Palm", "Red-tailed Black"],
         },
