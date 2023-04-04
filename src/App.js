e17a674b3ff1bfea83777a1b12567f414adc3b20
LBoalich <lkboalich@gmail.com>
2023-04-04 19:19:03 -0400

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import './App.css';
 import { Quiz } from './features/quiz/Quiz';
 import { Results } from './features/results/Results';
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={ <Results />} />
+));
+
 function App() {
   return (
     <div className="App">
-      <Results />
-      <Quiz />
+      <RouterProvider router={ router } />
     </div>
   );
 }
