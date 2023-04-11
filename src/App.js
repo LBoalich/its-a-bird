27567af8d3b64e87d428e9044eaab06c9341b9c4
LBoalich <lkboalich@gmail.com>
2023-04-11 14:08:12 -0400

    Add button to Result that leads to Description

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Root from './features/root/Root';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={ <Root />} >
-    <Route path="" element={ <Quiz />} />
+    <Route path="quiz" element={ <Quiz />} />
     <Route path="result" element={ <Results />} />
     <Route path="types" />
     <Route path="types:type" element={<Descriptions />} />
--- src/features/results/Results.js
+++ src/features/results/Results.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { selectResults, selectFinalAnswer } from './resultsSlice';
 
 export function Results() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const results = useSelector(selectResults);
     const finalAnswer = useSelector(selectFinalAnswer);
     const finalResult = results[finalAnswer];
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        navigate(`/types/${finalAnswer.replaceAll(" ", "-")}`)
+    };
 
     return (
         <div>
             <h1>You are most like the {finalAnswer} Cockatoo!</h1>
             <img src={require(`${finalResult.img}`)} alt={finalResult.alt}/>
+            <button onClick={handleClick}>Learn More</button>
         </div>
     );
 };
