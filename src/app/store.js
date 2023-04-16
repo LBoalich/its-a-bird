e7ca7ac754d74102f1450cb7b732229ba537f8cc
LBoalich <lkboalich@gmail.com>
2023-04-16 16:21:33 -0400

    Finish Results layout

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from 'redux-persist';
+import thunk from "redux-thunk";
 import rootReducer from './rootReducer';
 
 const persistConfig = {
@@ -12,6 +13,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export const persistor = persistStore(store);
--- src/features/results/Results.js
+++ src/features/results/Results.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { selectResults, selectFinalAnswer } from './resultsSlice';
+import "./Results.css";
 
 export function Results() {
     const navigate = useNavigate();
@@ -9,6 +10,7 @@ export function Results() {
     const results = useSelector(selectResults);
     const finalAnswer = useSelector(selectFinalAnswer);
     const finalResult = results[finalAnswer];
+    
 
     const handleClick = (e) => {
         e.preventDefault();
@@ -16,10 +18,14 @@ export function Results() {
     };
 
     return (
-        <div>
+        <section className="result-container">
             <h1>You are most like the {finalAnswer} Cockatoo!</h1>
-            <img src={require(`${finalResult.img}`)} alt={finalResult.alt}/>
-            <button onClick={handleClick}>Learn More</button>
-        </div>
+            <figure className="result-img-container" key={finalResult.alt}>
+                <img className="result-img" src={require(`${finalResult.img}`)} alt={finalResult.alt}/>
+            </figure>
+            <div className="button-div">
+                <button className="type-button" onClick={handleClick}>Learn More</button>
+            </div>
+        </section>
     );
 };
