4c51a9915c1331d319a9ee8150ea70150b30787d
LBoalich <lkboalich@gmail.com>
2023-04-16 20:13:49 -0400

    Finish Types layout

--- src/features/types/Types.js
+++ src/features/types/Types.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { selectTypes } from "./typesSlice";
+import "./Types.css";
 
 export function Types() {
     const slugify = require("slugify");
@@ -12,9 +13,14 @@ export function Types() {
         <section className="types-container">
             <h1>Cockatoo Types</h1>
             {typesArr.map(type => {
-                return <article className="type-container">
-                            <Link to={`/types/${slugify(type[0])}`}>{type[0]}</Link>
-                            <img src={require(`${types[type[0]].img}`)} alt={types[type[0]].alt}/>
+                return <article className="type-container" key={type[0]}>
+                            <h2>{type[0]}</h2>
+                            <figure className="type-img-container" key={types[type[0]].alt}>
+                                <img className="type-img" src={require(`${types[type[0]].img}`)} alt={types[type[0]].alt}/>
+                            </figure>
+                            <div className="learn-link-container">
+                                <Link to={`/types/${slugify(type[0])}`} className="learn-link">Learn More</Link>
+                            </div>
                         </article>
             })}
         </section>
