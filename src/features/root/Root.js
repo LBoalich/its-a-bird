bc352643add776af670a5b0c937affa102906834
LBoalich <lkboalich@gmail.com>
2023-04-23 20:37:48 -0400

    Add ScrollRestoration

--- src/features/root/Root.js
+++ src/features/root/Root.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import NavBar from "../navBar/NavBar";
 
 const Root = () => {
@@ -7,6 +7,7 @@ const Root = () => {
         <>
             <NavBar />
             <Outlet />
+            <ScrollRestoration />
         </>
     )
 };
