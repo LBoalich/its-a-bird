b8eaa165fce0f5a083f48a8d04fe8646611b1e41
LBoalich <lkboalich@gmail.com>
2023-04-26 15:23:58 -0400

    Add line break between Home paragraphs

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -15,9 +15,11 @@ export const Home = () => {
                 </div>
                 <p> are the true rockstars of the bird world! These feathery divas love to headbang to their own beat, with their luscious crests bouncing in perfect rhythm. They're not just singers, they're screamers too! With their powerful lungs, they can belt out a tune that can be heard from miles away.
                 </p>
+                <br />
                 <img src={require("./homePics/dancing.jpg")} alt="Breakdancing Cockatoo" className="home-img" id="right"/>
                 <p> But that's not all - these quirky creatures have a knack for saying the funniest things! From mimicking human speech to coming up with their own unique phrases, they'll have you in stitches with their witty repertoire. Just make sure to turn up the music and watch them bust a move with their hilarious dance moves - you won't be able to resist joining in on the fun!
                 </p>
+                <br />
                 <img src={require("./homePics/singing.jpg")} alt="Singing Cockatoo" className="home-img" id="left-bottom"/>
                 <p> Cockatoos are truly the life of the party, and they'll leave you in awe with their comedic talents and contagious energy. Rock on, feathered friends!
                 </p>
