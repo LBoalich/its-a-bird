b4777efb3122b409a26f63c35e2e441dcb74c618
LBoalich <lkboalich@gmail.com>
2023-04-30 21:35:06 -0400

    Add responsive image sizes to Results

--- src/features/results/Results.js
+++ src/features/results/Results.js
@@ -15,9 +15,25 @@ export function Results() {
         return (
             <section className="no-result-container">
                 <h1>Want to know which cockatoo you are most like?</h1>
-                <figure className="no-result-img-container">
-                    <img src={require("./resultsPics/cute.webp")} alt="Two cute multi-colored cartoon cockatoos" className="no-result-img" />
-                </figure>
+                <picture className="no-result-img-container">
+                    <source 
+                        srcSet={require("./resultsPics/cute80.webp")}
+                        media="(min-width: 1601px)" 
+                    />
+                    <source 
+                        srcSet={require("./resultsPics/cute60.webp")}
+                        media="(min-width: 1025px)"
+                    />
+                    <source 
+                        srcSet={require("./resultsPics/cute40.webp")}
+                        media="(min-width: 747px)"
+                    />
+                    <source 
+                        srcSet={require("./resultsPics/cute30.webp")}
+                        media="(min-width: 0px)"
+                    />
+                    <img src={require("./resultsPics/cute30.webp")} alt="Two cute multi-colored cartoon cockatoos" className="no-result-img" />
+                </picture>
                 <h2>Take the <Link to="/quiz" className="no-result-link">QUIZ</Link> now to see your result!</h2>
             </section>
         )
@@ -31,9 +47,25 @@ export function Results() {
     return (
         <section className="result-container">
             <h1>You are most like the {finalAnswer} Cockatoo!</h1>
-            <figure className="result-img-container" key={finalResult.alt}>
-                <img className="result-img" src={require(`${finalResult.img}`)} alt={finalResult.alt}/>
-            </figure>
+            <picture className="result-img-container">
+                <source 
+                    srcSet={require(`${finalResult.img80}`)}
+                    media="(min-width: 1601px)" 
+                />
+                <source 
+                    srcSet={require(`${finalResult.img60}`)}
+                    media="(min-width: 1025px)"
+                />
+                <source 
+                    srcSet={require(`${finalResult.img40}`)}
+                    media="(min-width: 747px)"
+                />
+                <source 
+                    srcSet={require(`${finalResult.img30}`)}
+                    media="(min-width: 0px)"
+                />
+                <img className="result-img" src={require(`${finalResult.img40}`)} alt={finalResult.alt}/>
+            </picture>
             <div className="button-div">
                 <button className="type-button" onClick={handleClick}>Learn More</button>
             </div>
