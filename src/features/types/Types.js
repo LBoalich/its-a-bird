fb7eb5f821cfd85c7123ec8545eb6bebff24ef34
LBoalich <lkboalich@gmail.com>
2023-05-04 12:12:13 -0400

    Add 2x dpr images to Types

--- src/features/types/Types.js
+++ src/features/types/Types.js
@@ -15,27 +15,27 @@ export function Types() {
                 className="types-font-container"
             >
                 <source 
-                    srcSet={require("./typesPics/cockatoo-types-80.webp")} 
+                    srcSet={require("./typesFont/cockatoo-types-80.webp")} 
                     media="(min-width: 1601px)"
                     alt="feathered font reading 'Cockatoo Types'"
                 />
                 <source 
-                    srcSet={require("./typesPics/cockatoo-types-60.webp")} 
+                    srcSet={require("./typesFont/cockatoo-types-60.webp")} 
                     media="(min-width: 1025px)"
                     alt="feathered font reading 'Cockatoo Types'"
                 />
                 <source 
-                    srcSet={require("./typesPics/cockatoo-types-40.webp")} 
+                    srcSet={require("./typesFont/cockatoo-types-40.webp")} 
                     media="(min-width: 747px)"
                     alt="feathered font reading 'Cockatoo Types'" 
                 />
                 <source 
-                    srcSet={require("./typesPics/cockatoo-types-20.webp")} 
+                    srcSet={require("./typesFont/cockatoo-types-20.webp")} 
                     media="(min-width: 0px)"
                     alt="feathered font reading 'Cockatoo Types'" 
                 />
                 <img 
-                    src={require("./typesPics/cockatoo-types-40.webp")} 
+                    src={require("./typesFont/cockatoo-types-40.webp")} 
                     alt="feathered font reading 'Cockatoo Types'" className="types-font-img"
                 />
             </picture>
@@ -43,12 +43,27 @@ export function Types() {
             {typesArr.map(type => {
                 return <article className="type-container" key={type[0]}>
                             <h2>{type[0]}</h2>
-                            <figure className="type-img-container" key={types[type[0]].alt}>
-                                <img className="type-img" 
-                                src={require(`${types[type[0]].img}`)} alt={types[type[0]].alt}
+                            <picture
+                                className="type-img-container" 
+                            >
+                                <source 
+                                    media="(min-width: 1600px)"
+                                    srcSet={`${require(`${types[type[0]].imgM}`)}, ${require(`${types[type[0]].imgL}`)} 2x`}
+                                />
+                                <source 
+                                    media="(min-width: 768px)"
+                                    srcSet={`${require(`${types[type[0]].imgS}`)}, ${require(`${types[type[0]].imgM}`)} 2x`}
+                                />
+                                <source 
+                                    media="(min-width: 0px)"
+                                    srcSet={`${require(`${types[type[0]].imgXS}`)}, ${require(`${types[type[0]].imgS}`)} 2x`}
+                                />
+                                <img className="type-img"
+                                src={require(`${types[type[0]].imgS}`)} 
+                                alt={types[type[0]].alt}
                                 fetchpriority={type[0] === "Blue-eyed" ? "high" : "low"}
                             />
-                            </figure>
+                            </picture>
                             <button className="learn-link-container">
                                 <Link to={`/types/${slugify(type[0])}`} className="learn-link">Description</Link>
                             </button>
